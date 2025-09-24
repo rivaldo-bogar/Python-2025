152d94cb97e2b9a1b1b4a719fd02e9f6969d1089
rivaldo-bogar <androsbgr13@gmail.com>
2025-09-24 22:05:07 +0900

    Last topic concatenateString.py

---
+++ concatenateString.py
@@ -0,0 +1,11 @@
+v1 = "Good"
+v2 = "Night guyss"
+car_name = ['avanza', 'terios', 'ferari', 'lamborgini', 'kayla', 'ayla', 'datsun', 'wuling']
+gabungkan = " ".join(car_name) # contoh baru menggabungkan string
+gabungkan2 = '{} {}'.format(v1,v2)
+print(f"{gabungkan}")
+print(f" dengan type berbeda : {gabungkan2}")
+print(v1 +  " " +  v2)
+
+# BEIRKUT DI ATAS INI CARA MENGGABUNGKAN STRING DENGAN BEBERAPA CARA
+
---
+++ terminal_demo/list_efisiensi.py
@@ -0,0 +1,3 @@
+variabel_name = str(input("name variabel : "))
+input_user = str(input("masukan string dengan jarak space : "))
+print(f"{variabel_name} = {input_user.split(" ")}")
