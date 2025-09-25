711f21c2c0c9db4ad2c4c4489b7d3b105f0da0f4
rivaldo-bogar <androsbgr13@gmail.com>
2025-09-25 22:16:55 +0900

    adding new topic formatString.py

---
+++ formatString.py
@@ -0,0 +1,10 @@
+# Topic ini mengenai format penggunaan string
+age = 36
+txt = "Nama saya rivaldo bogar, saya adalah seorang programmer Artificial Intelegence di indonesia,sampingan saya yaitu sebagai investor cyrpto"
+# yang di atas ini menggunakan cara sederhana
+print(txt)
+
+# cara menggunakan f-string
+txt = f"Nama saya rivaldo bogar, saya adalah seorang programmer Artificial Intelegence di indonesia,sampingan saya yaitu sebagai investor cyrpto number manipulation {age}"
+print(txt)
+print(f"Hargan : Rp.{age:.3f}") #untuk menambah berapa digit 0,dibelakang koma atau titik.dari bilangan real
